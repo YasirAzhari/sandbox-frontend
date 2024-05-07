c7350a9c34abe1dbac8723968b0ef3b40656feca
YasirAzhari <grabuser2000@gmail.com>
2024-05-07 14:41:22 +0400

    Radix ui stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import '@radix-ui/themes/styles.css';
+import { Theme, ThemePanel } from '@radix-ui/themes';
+import { Flex, Text, Tabs, Box, Container, Section } from '@radix-ui/themes';
+import Home from './Home';
 
 function App() {
 
   return (
     <>
+      <Theme appearance="dark" accentColor="gray">
+    
+        <Flex direction="column" gap="1">
+
+          <Tabs.Root defaultValue="Home">
+            <Container size={'4'}>
+              <Tabs.List>
+                <Tabs.Trigger value="Home">Home</Tabs.Trigger>
+                <Tabs.Trigger value="About me">About me</Tabs.Trigger>
+              </Tabs.List>
+            
+              <Box pt="3">
+                <Tabs.Content value="Home">
+                  <Section>
+                    <Home/>
+                  </Section>
+                </Tabs.Content>
+
+                <Tabs.Content value="About me">
+                  <Text size="2">Access and update your documents.</Text>
+                </Tabs.Content>
+              </Box>
+            </Container>
+          </Tabs.Root>
+        </Flex>
+        <ThemePanel/>
+      </Theme>
     </>
   )
 }
---
+++ src/Home.jsx
@@ -0,0 +1,38 @@
+import { Flex, Text, Button, Tabs, Heading, Box, RadioCards, Container, Section, Grid, AlertDialog } from '@radix-ui/themes';
+import sand from './assets/sand.png'
+
+export default function Home() {
+  return (
+    <Grid columns={"2"}>
+        <Flex direction={"column"} gap={"8"}>
+            <Heading size={"9"}>Hello.</Heading>
+            <Text >I'm gonna be dumping all the little thingamajigs, trinkets, doo-dads and whatcha-ma-call-its that I come up with into this here website. I'll just add a new tab whenever something comes up.</Text>
+            <Box>
+            <AlertDialog.Root>
+            <AlertDialog.Trigger>
+                <Button >Contact me!</Button>
+            </AlertDialog.Trigger>
+            <AlertDialog.Content maxWidth="450px">
+                <AlertDialog.Title>Fuck You</AlertDialog.Title>
+                <AlertDialog.Description size="2">
+                Don't you EVER try to contact me again. Who do you think you are?
+                </AlertDialog.Description>
+
+                <Flex gap="3" mt="4" justify="end">
+                <AlertDialog.Cancel>
+                    <Button variant="soft" color="gray">
+                    Sorry
+                    </Button>
+                </AlertDialog.Cancel>
+                </Flex>
+            </AlertDialog.Content>
+            </AlertDialog.Root>
+            </Box>
+        </Flex>
+        <Box >
+            <img src= {sand} width={'500px'}/>
+        </Box>
+        
+    </Grid>
+  )
+}
